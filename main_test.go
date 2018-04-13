803cc730b7d4025b811732fef9ac26f0b0e4e2ba
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2018-04-13 12:23:08 +0530

    Refactor code and moved functionality
    The setup database function is not required since it's not what we are doing multiple times,
    the database string is a constant so it has been declared that way.
    
    Signed-off-by: Farhaan Bukhsh <farhaan.bukhsh@gmail.com>

--- main_test.go
+++ main_test.go
@@ -6,46 +6,12 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 )
-
-// Test that database is setup properly.
-func TestSetupDatabase(t *testing.T) {
-	databaseName := "test.db"
-	// First, we setup the database.
-	setupDatabase(databaseName)
-	// Check the sqlite database file was created.
-	if _, err := os.Stat(databaseName); os.IsNotExist(err) {
-		t.Errorf("Database wasn't created, %s sqlite database doesn't exist.", databaseName)
-	}
-
-	// Test that we can open the database that was created.
-	db, err := gorm.Open("sqlite3", databaseName)
-	if err != nil {
-		t.Errorf("Couldn't read the database %s", databaseName)
-		return
-	}
-
-	// Test that models were migrated.
-	db.Create(&Link{Short: "test", Url: "http://example.com"})
-
-	// Test that we can read the database.
-	var link Link
-	db.First(&link, 1)
-	if link.Url != "http://example.com" {
-		t.Errorf("Wrong value read from database, expecting http://exmaple.com, got %v", link.Url)
-	}
-
-	// Cleanup the database.
-	os.Remove(databaseName)
-
-}
 
 // Test Index page handler.
 func TestIndexPage(t *testing.T) {
 	databaseName := "test.db"
-	setupDatabase(databaseName)
 
 	router := setupRouter()
 
